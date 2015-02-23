bb197a436d41854b271c0cf2ea2ed7af0d34042a
marc-dev <marc.viguie@heh.be>
2015-02-23 16:34:17 +0100

    remove ? maybe

---
+++ Unity/Concept_1_MiniGame/Meduse/Assets/Scripts/Misc/Border.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Border : MonoBehaviour
+ {
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		Destroy(col.gameObject);
+	//	col.gameObject.die();//		Destroy(col.gameObject);
+	}
+}
